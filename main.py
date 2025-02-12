ea7831a09a4bdac9655b8062646fadf4a4f67c03
HrsHristov <hrshristov@gmail.com>
2025-02-12 19:51:41 +0000

    rename logger.py to utils.py

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from binance_client import get_historical_data
+import utils
 
 def main():
     
+    # Set up logging
+    logger = utils.setup_logger("main")
+    logger.info("Starting Risk To Reward Calculator")
     #Fetch historical data from Binance
     data = get_historical_data()
     print(data)
